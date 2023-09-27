e73b588902dd30032fe8d058ec01670209fe0081
oracledbms2023 <140682561+oracledbms2023@users.noreply.github.com>
2023-09-27 07:12:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+function handleCopy1a (){
+     let text = document.getElementById('text1a');
+     text = text.textContent;
+     navigator.clipboard.writeText(text);
+     console.log("1a");
+   }
+   function handleCopy2a (){
+    let text = document.getElementById('text2a');
+    text = text.textContent;
+    navigator.clipboard.writeText(text);
+    console.log("2a");
+  }
+
+  function handleCopy1b (){
+    let text = document.getElementById('text1b');
+    text = text.textContent;
+    navigator.clipboard.writeText(text);
+    console.log("1b");
+  }
+
+  function handleCopy2b (){
+    let text = document.getElementById('text2b');
+    text = text.textContent;
+    navigator.clipboard.writeText(text);
+    console.log("2b");
+  }
+
+  function handleCopy3a (){
+    let text = document.getElementById('text3a');
+    text = text.textContent;
+    navigator.clipboard.writeText(text);
+    console.log("3a");
+  }
+
+  function handleCopy3b (){
+    let text = document.getElementById('text3b');
+    text = text.textContent;
+    navigator.clipboard.writeText(text);
+    console.log("3b");
+  }
+  function handleCopy4a (){
+    let text = document.getElementById('text4a');
+    text = text.textContent;
+    navigator.clipboard.writeText(text);
+    console.log("4a");
+
+  } 
+   function handleCopy4b (){
+    let text = document.getElementById('text4b');
+    text = text.textContent;
+    navigator.clipboard.writeText(text);
+    console.log("4b");
+  }  
+  function handleCopy4c (){
+    let text = document.getElementById('text4c');
+    text = text.textContent;
+    navigator.clipboard.writeText(text);
+    console.log("4c");
+    
+  }function handleCopy5a1 (){
+    let text = document.getElementById('text5a1');
+    text = text.textContent;
+    navigator.clipboard.writeText(text);
+    console.log("5a1");
+  }  function handleCopy5a2 (){
+    let text = document.getElementById('text5a2');
+    text = text.textContent;
+    navigator.clipboard.writeText(text);
+    console.log("5a2");
+  }
+  function handleCopy5b1 (){
+    let text = document.getElementById('text5b1');
+    text = text.textContent;
+    navigator.clipboard.writeText(text);
+    console.log("5b1");
+  }
